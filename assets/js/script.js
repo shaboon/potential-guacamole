645437f4c75aac5d71a741de482bd9aa85cdc731
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-03-20 10:51:10 -0400

    add localButton save functionality, not pulling yt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var date = document.querySelector("#currentDay");
-var today = dayjs().format("MMM D, YYYY");
-$(date).text(today);
 
 $(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
+  $(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+
+    localStorage.setItem(hour, JSON.stringify(task));
+  });
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -14,14 +17,52 @@ $(document).ready(function () {
   // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time
+
+  function whatTime() {
+    var currentHour = dayjs.format("H");
+    console.log(currentHour);
+  }
+
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  $("hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("hour-18 .description").val(localStorage.getItem("hour-18"));
+  $("hour-19 .description").val(localStorage.getItem("hour-19"));
+  $("hour-20 .description").val(localStorage.getItem("hour-20"));
+  $("hour-21 .description").val(localStorage.getItem("hour-21"));
+  $("hour-22 .description").val(localStorage.getItem("hour-22"));
+  $("hour-23 .description").val(localStorage.getItem("hour-23"));
+  $("hour-24 .description").val(localStorage.getItem("hour-24"));
+  $("hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("hour-2 .description").val(localStorage.getItem("hour-2"));
+  $("hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("hour-5 .description").val(localStorage.getItem("hour-5"));
+  $("hour-6 .description").val(localStorage.getItem("hour-6"));
+  $("hour-7 .description").val(localStorage.getItem("hour-7"));
+  $("hour-8 .description").val(localStorage.getItem("hour-8"));
+
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  var date = document.querySelector("#currentDay");
+  var today = dayjs().format("MMM D, YYYY");
+  date.textContent = today;
+
+  whatTime();
 });
